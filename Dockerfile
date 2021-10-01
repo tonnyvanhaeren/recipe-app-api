5580c72ecd9a4207e5995087a4228e2ae691bcaa
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-10-01 08:44:08 +0200

    init restart

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.9-alpine
+
+LABEL email="antonius.vanhaeren.av@gmail.com"
+
+ENV PYTHONUNBUFFERED 1
+
+COPY ./setup.cfg /setup.cfg
+COPY ./requirements.txt /requirements.txt
+
+
+RUN apk add --update --no-cache postgresql-client
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+    gcc libc-dev linux-headers postgresql-dev
+RUN pip install -r /requirements.txt
+RUN apk del .tmp-build-deps
+
+RUN mkdir /app
+WORKDIR /app
+COPY ./app /app
+
+RUN adduser -D user
+USER user
---
+++ app/core/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        """create and save a new user"""
+        if not email:
+            raise ValueError("User must have a email address")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and saves a new super user"""
+        super_user = self.create_user(email, password)
+
+        super_user.is_staff = True
+        super_user.is_superuser = True
+
+        super_user.save(using=self._db)
+
+        return super_user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """custom user model that support using email instead of username"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
