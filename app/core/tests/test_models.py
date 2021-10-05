0618954d76fbb76bde548579a15a04f53de740ae
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-10-05 08:15:27 +0200

    Added tags list endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,14 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+from core import models
+
+
+def sample_user(email="test@example.com", password="password123"):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a new user with email is successful"""
@@ -40,3 +48,12 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name="Vegan",
+        )
+
+        self.assertEqual(str(tag), tag.name)
---
+++ app/recipe/views.py
@@ -0,0 +1,19 @@
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Tag
+from recipe import serializers
+
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage atgs in the database"""
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return only objects for the current authentcated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
