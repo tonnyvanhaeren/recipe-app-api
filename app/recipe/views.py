5560223e2f89344de4fa0c6e23d9bbcb0b0ff393
tonnyvanhaeren <antonius.vanhaeren.av@gmail.com>
2021-10-06 10:30:54 +0200

    Adedd List ingredients feature

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,17 +2,34 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 from recipe import serializers
 
 
 class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-    """Manage atgs in the database"""
+    """Manage tags in the database"""
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return only objects for the current authentcated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        """Create a new Tag"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    """Manage ingredients in the database"""
+
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         """Return only objects for the current authentcated user"""
